92f2c10407a6054d18156975a6988a02b4dc0e44
Andrey Krivosheev <feuiuxbe@gmail.com>
2022-03-25 19:26:33 +0100

    feat: add lesson02 solution

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
-let title, screens, screenPrice, rollback, fullPrice, adaptive;
-alert('Hello JavaScript');
-console.log('Hello JS user');
+let title = 'My JS Project';
+let screens = "Простые, Сложные, Интерактивные";
+let screenPrice = 1000;
+let rollback = 20;
+let fullPrice = 2000;
+let adaptive = true;
+
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+
+console.log(screens.length);
+
+console.log(`Стоимость верстки экранов ${screenPrice} евро`);
+console.log(`Стоимость разработки сайта ${fullPrice} евро`);
+
+console.log(screens.toLowerCase().split(', '));
+
+console.log(`Процент отката посреднику за работу ` + (fullPrice * (rollback/100)));
